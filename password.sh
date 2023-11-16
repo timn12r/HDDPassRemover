645c165e93bd81a41d61bcd0868feb9fe6050ee9
timn12r <120687923+timn12r@users.noreply.github.com>
2023-11-15 18:24:39 -0700

    Coloring updates

--- password.sh
+++ password.sh
@@ -1,15 +1,18 @@
 #Debug Colors
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 RED='\033[1;31m'
+GREEN='\033[0;32m'
+BLUE='\033[1:34m'
+YELLOW='\033[1;33m'
 CLEAR='\033[0;0m'
-
+#Summary variables
 passed_drives=0
 failed_drives=0
 
+#Refresh drive list 
 echo -e "${YELLOW} Refreshing drive list...${CLEAR}"
 sudo rescan-scsi-bus
 
+#Obtain drive count
 drive_count=$(lsblk -o NAME | grep -E '^sd' | awk 'END {print NR}')
 echo -e "${YELLOW}Drive count: $drive_count ${CLEAR}"
 if [ "$drive_count" -lt 2 ]; then
@@ -18,28 +21,27 @@ if [ "$drive_count" -lt 2 ]; then
 else
     echo -e "${GREEN}New drives found.${CLEAR}"
 fi
-
+#revise boot drive later
 boot_drive="sda"
 new_drives=$(lsblk -o NAME | grep -E '^sd' | grep -v "$boot_drive")
-echo -e "${YELLOW}Found drives: $new_drives ${CLEAR}"
+echo -e "${YELLOW}Found drives:\n${BLUE}$new_drives ${CLEAR}"
 
 for drive in $new_drives; do
-    echo -e "${YELLOW}Attempting to remove password from drive $drive...${CLEAR}"
+    echo -e "${YELLOW}Attempting to remove password from drive ${BLUE}$drive${YELLOW}...${CLEAR}"
     # Attempt to remove the password
     remove_pass=$(sudo hdparm --user-master u --security-set-pass pass /dev/$drive 2>&1)
 
     # Check the exit status of the hdparm command
     if echo "$remove_pass" | grep -q "bad"; then
-        echo -e "${RED}Drive $drive FAILED to unlock. Please reboot with Space Monkey.${CLEAR}"
+        echo -e "${RED}Drive ${BLUE}$drive${RED} FAILED to unlock. Please reboot with Space Monkey.${CLEAR}"
         ((failed_drives++))
     else
-        echo -e "${GREEN}Drive $drive UNLOCKED.${CLEAR}"
+        echo -e "${GREEN}Drive ${BLUE}$drive${GREEN} UNLOCKED.${CLEAR}"
         # Disable security
         sudo hdparm --security-disable pass /dev/$drive
         ((passed_drives++))
     fi
 done
 
-echo -e "${GREEN}Drives PASSED: $passed_drives, ${RED}Drives FAILED: $failed_drives${CLEAR}"
-
+echo -e "${GREEN}Drives PASSED: ${BLUE}$passed_drives${CLEAR}, ${RED}Drives FAILED: ${BLUE}$failed_drives${CLEAR}"
 echo -e "${GREEN}------------DONE------------${CLEAR}"
